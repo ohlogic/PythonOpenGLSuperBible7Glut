a9adf6fe07e20c1b8f30fad212e0a2433e40080e
Stan S <vfpro777@yahoo.com>
2019-06-28 11:06:52 -0400

    added m3dLookAt function to sbmath.py

--- chapt05/shared/sbmath.py
+++ chapt05/shared/sbmath.py
@@ -135,3 +135,37 @@ def m3dScaleMatrix44(m, x, y, z):
     m[0] *= x
     m[5] *= y
     m[10] *= z
+
+
+
+def length(v):
+    return math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2])
+
+def normalize(v):
+    l = length(v)
+    #if (v[0] == 0 and v[1] == 0 and v[2] ==0):
+    #    return [0.0, 1/3, 0.0]
+    return [v[0]/l, v[1]/l, v[2]/l]
+    
+def dot(v0, v1):
+    return v0[0]*v1[0]+v0[1]*v1[1]+v0[2]*v1[2]
+
+def cross(v0, v1):
+    return [
+        v0[1]*v1[2]-v1[1]*v0[2],
+        v0[2]*v1[0]-v1[2]*v0[0],
+        v0[0]*v1[1]-v1[0]*v0[1]]
+
+def m3dLookAt(eye, target, up):
+    mz = normalize( (eye[0]-target[0], eye[1]-target[1], eye[2]-target[2]) ) # inverse line of sight
+    mx = normalize( cross( up, mz ) )
+    my = normalize( cross( mz, mx ) )
+    tx =  dot( mx, eye )
+    ty =  dot( my, eye )
+    tz = -dot( mz, eye )   
+    return np.array([mx[0], my[0], mz[0], 0, mx[1], my[1], mz[1], 0, mx[2], my[2], mz[2], 0, tx, ty, tz, 1])
+
+def scale(s):
+    return [s,0,0,0, 0,s,0,0, 0,0,s,0, 0,0,0,1]     
+    
+    
